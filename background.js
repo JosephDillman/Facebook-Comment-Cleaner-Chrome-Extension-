c8c14e0243b9bd05d73e4a194a89a81f659e6c5e
Joseph Dillman <josephgdillman@gmail.com>
2019-09-02 11:24:49 -0700

    organized pictures into folder

--- background.js
+++ background.js
@@ -6,9 +6,9 @@ chrome.runtime.onInstalled.addListener(function() {
 chrome.runtime.onMessage.addListener(function (msg, sender, sendResponse) {
     if (msg.action === "iconRed") {
         if (msg.value) {
-            chrome.browserAction.setIcon({path: "/logo128Red.png"});
+            chrome.browserAction.setIcon({path: "/pics/logo128Red.png"});
         }else {
-            chrome.browserAction.setIcon({path: "/logo128.png"});
+            chrome.browserAction.setIcon({path: "/pics/logo128.png"});
         }
     }
 });
